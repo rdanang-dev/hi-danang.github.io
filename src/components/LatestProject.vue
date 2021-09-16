01f0bf642941fbb9036824701ff5a1f914ee6649
RDanang <rdanang.dev@gmail.com>
2021-09-16 11:25:35 +0700

    [Update] change layout

--- src/components/LatestProject.vue
+++ src/components/LatestProject.vue
@@ -25,83 +25,78 @@
 
       <!-- Projects -->
       <div
-        class="flex flex-col w-full"
+        class="pt-8 flex flex-col rounded-t-3xl rounded-b-3xl"
+        :class="
+          (index + 1) % 2 === 0 ? ['lg:flex-row-reverse'] : ['lg:flex-row']
+        "
         v-for="(value, index) in data"
         :key="index"
       >
-        <div class="pt-8">
-          <div
-            class="flex flex-col rounded-t-3xl rounded-b-3xl"
+        <a :href="value.links ? value.links : value.demo" target="_blank">
+          <img
+            :src="require(`@/assets/image/projects/${value.image}`)"
+            class="w-full h-full rounded-t-3xl border border-green-500 object-center"
             :class="
-              (index + 1) % 2 === 0 ? ['lg:flex-row-reverse'] : ['lg:flex-row']
+              (index + 1) % 2 === 0
+                ? ['lg:rounded-br-3xl lg:rounded-t-none lg:rounded-tr-3xl']
+                : ['lg:rounded-bl-3xl lg:rounded-t-none lg:rounded-tl-3xl']
             "
+          />
+        </a>
+        <div
+          class="flex flex-col px-3 pt-2 border-dashed lg:border-t border-b border-l border-r rounded-bl-3xl rounded-br-3xl border-green-500 relative lg:w-7/12"
+          :class="
+            (index + 1) % 2 === 0
+              ? [
+                  'lg:border-l lg:rounded-tl-3xl lg:rounded-bl-3xl lg:rounded-br-none lg:border-r-0',
+                ]
+              : [
+                  'lg:border-r lg:rounded-tr-3xl lg:rounded-br-3xl lg:rounded-bl-none lg:border-l-0',
+                ]
+          "
+        >
+          <span class="text-custom-title text-2xl">{{ value.title }}</span>
+          <div
+            class="text-justify leading-tight tracking-tighter lg:leading-normal py-2 lg:pt-5 text-custom-content"
           >
-            <img
-              :src="require(`@/assets/image/projects/${value.image}`)"
-              class="w-full h-full lg:w-9/12 relative rounded-t-3xl border border-green-500"
-              :class="
-                (index + 1) % 2 === 0
-                  ? ['lg:rounded-br-3xl lg:rounded-t-none lg:rounded-tr-3xl']
-                  : ['lg:rounded-bl-3xl lg:rounded-t-none lg:rounded-tl-3xl']
-              "
-              @click="redirect(value.links ? value.links : value.demo)"
-            />
-            <div
-              class="flex flex-col px-3 pt-2 border-dashed lg:border-t border-b border-l border-r rounded-bl-3xl rounded-br-3xl border-green-500 relative"
-              :class="
-                (index + 1) % 2 === 0
-                  ? [
-                      'lg:border-l lg:rounded-tl-3xl lg:rounded-bl-3xl lg:rounded-br-none lg:border-r-0',
-                    ]
-                  : [
-                      'lg:border-r lg:rounded-tr-3xl lg:rounded-br-3xl lg:rounded-bl-none lg:border-l-0',
-                    ]
-              "
-            >
-              <span class="text-custom-title text-2xl">{{ value.title }}</span>
-              <div
-                class="text-justify leading-tight tracking-tighter lg:leading-normal py-2 lg:pt-5 text-custom-content"
+            <p>{{ value.desc }}</p>
+            <div class="space-x-1 space-y-1 max-w-64 pb-14 lg:pb-0">
+              <span class="block py-2">Tags:</span>
+              <span
+                v-for="tag in value.tags"
+                :key="tag"
+                class="inline-block border border-dotted border-green-500 tracking-normal px-2"
+                >{{ tag }}</span
               >
-                <p>{{ value.desc }}</p>
-                <div class="space-x-1 space-y-1 max-w-64 pb-14 lg:pb-0">
-                  <span class="block py-2">Tags:</span>
-                  <span
-                    v-for="tag in value.tags"
-                    :key="tag"
-                    class="inline-block border border-dotted border-green-500 tracking-normal px-2"
-                    >{{ tag }}</span
-                  >
-                </div>
-              </div>
-              <div
-                class="flex justify-end p-2 absolute bottom-0 right-2 space-x-2 text-green-500"
-              >
-                <a :href="value.links" target="_blank" v-if="value.links">
-                  <font-awesome-icon
-                    :icon="['fas', 'external-link-square-alt']"
-                    class="fa-2x"
-                  />
-                </a>
-                <a :href="value.frontend" target="_blank" v-if="value.frontend">
-                  <font-awesome-icon
-                    :icon="['fab', 'github-square']"
-                    class="fa-2x"
-                  />
-                </a>
-                <a :href="value.backend" target="_blank" v-if="value.backend">
-                  <font-awesome-icon
-                    :icon="['fab', 'github-square']"
-                    class="fa-2x"
-                  />
-                </a>
-                <a :href="value.demo" target="_blank" v-if="value.demo">
-                  <font-awesome-icon
-                    :icon="['fab', 'youtube-square']"
-                    class="fa-2x"
-                  />
-                </a>
-              </div>
             </div>
+          </div>
+          <div
+            class="flex justify-end p-2 absolute bottom-0 right-2 space-x-2 text-green-500"
+          >
+            <a :href="value.links" target="_blank" v-if="value.links">
+              <font-awesome-icon
+                :icon="['fas', 'external-link-square-alt']"
+                class="fa-2x"
+              />
+            </a>
+            <a :href="value.frontend" target="_blank" v-if="value.frontend">
+              <font-awesome-icon
+                :icon="['fab', 'github-square']"
+                class="fa-2x"
+              />
+            </a>
+            <a :href="value.backend" target="_blank" v-if="value.backend">
+              <font-awesome-icon
+                :icon="['fab', 'github-square']"
+                class="fa-2x"
+              />
+            </a>
+            <a :href="value.demo" target="_blank" v-if="value.demo">
+              <font-awesome-icon
+                :icon="['fab', 'youtube-square']"
+                class="fa-2x"
+              />
+            </a>
           </div>
         </div>
       </div>
@@ -117,11 +112,6 @@
       return {
         data: projectData.data,
       }
-    },
-    methods: {
-      redirect(value, target = "_blank") {
-        window.open = (value, target)
-      },
     },
   }
 </script>
