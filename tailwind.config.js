05178c9808223e57b7f75b678b173e50cb848403
rdanang-dev <rdanang.dev@gmail.com>
2022-01-12 04:33:23 +0700

    try jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
     extend: {
       colors: {
